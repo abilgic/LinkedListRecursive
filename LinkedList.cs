349edb4c9e3bfbadc2c2bd9e0884bcc173653db0
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-06-29 14:43:08 +0300

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+namespace LinkedListRecursive
+{
+    internal class LinkedList
+    {
+        public class Node
+        {
+            public int Data;
+            public Node(int Data) { this.Data = Data; }
+            public Node next;
+        }
+
+        public Node Insert(Node head, int Data)
+        {
+            if (head == null)
+            {
+                var newnode = new Node(Data);
+                head = newnode;
+                return newnode;
+            }
+            else
+            {
+                var current = head;
+
+                head.next = Insert(head.next, Data);
+                return head;
+
+            }
+        }
+
+        public void Print(Node head)
+        {
+            if (head == null)
+                return;
+            Console.Write($"{head.Data} ");
+            Print(head.next);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using static LinkedListRecursive.LinkedList;
+
+Node head = null;
+var linkedlist = new LinkedListRecursive.LinkedList();
+head = linkedlist.Insert(head, 1);
+head = linkedlist.Insert(head, 2);
+head = linkedlist.Insert(head, 3);
+head = linkedlist.Insert(head, 4);
+head = linkedlist.Insert(head, 5);
+linkedlist.Print(head);
